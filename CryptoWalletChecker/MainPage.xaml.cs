8ebc3e66424c5b11777b0c3fe15324ff32ff9d47
KoletoG <twindestroyersbgbg@gmail.com>
2025-04-01 16:46:47 +0300

    chore: Created a method for checking if wallet exists
    Created a method for checking if wallet had been previously added.

--- CryptoWalletChecker/MainPage.xaml.cs
+++ CryptoWalletChecker/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace CryptoWalletChecker
+﻿using System.Runtime.InteropServices.Marshalling;
+
+namespace CryptoWalletChecker
 {
     public partial class MainPage : ContentPage
     {
@@ -6,13 +8,32 @@
 
         public MainPage()
         {
-            InitializeComponent();
+            InitializeComponent(); 
+            using (StreamWriter streamWriter = new StreamWriter(@"..\..\wallets.txt", true))
+            {
+                Console.WriteLine("Wallets.txt has been created");
+            }
         }
 
         private void OnSubmitClicked(object sender, EventArgs e)
         {
             string userInput = textInput.Text;
             DisplayAlert("Input Received", $"You entered: {userInput}", "OK");
+        }
+        private bool IsWalletExists(string wallet)
+        {
+            bool exists = false;
+            using (StreamReader str = new StreamReader(@"..\..\wallets.txt"))
+            {
+                while (!str.EndOfStream) 
+                {
+                    if (str.ReadLine() == wallet)
+                    {
+                        exists = true;
+                    }
+                }
+            }
+            return exists;
         }
     }
 
