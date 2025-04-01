a37843c1c3b7aad6033d6777f2304eb96cf7cd91
KoletoG <twindestroyersbgbg@gmail.com>
2025-04-01 16:28:02 +0300

    Add project files.

---
+++ CryptoWalletChecker/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace CryptoWalletChecker
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSubmitClicked(object sender, EventArgs e)
+        {
+            string userInput = textInput.Text;
+            DisplayAlert("Input Received", $"You entered: {userInput}", "OK");
+        }
+    }
+
+}
